eefd6f5ca491f7950186baab5f08be6f688b9195
fylstathis3 <fylstathis@gmail.com>
2020-12-27 19:58:33 +0200

    Added endpoints for users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user wijth an email is susccessful"""
+        """Test creating a new user with an email is susccessful"""
         email = 'test@fylstathis.com'
         password = '2132Password'
         user = get_user_model().objects.create_user(
